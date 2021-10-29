290fa899805ff3c121958ff67594e21babf9e2e4
GNataliya <80199130+GNataliya@users.noreply.github.com>
2021-10-30 00:47:07 +0300

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,47 +1,14 @@
 const authEl = document.querySelector('.auth');
 const registrEl = document.querySelector('.signup');
 const registrFormShow = document.querySelector('.signUpFormBtn');
-// const linkSignIn = document.querySelector('.linkSignIn');
-
-// const checkUserAccess = async () => {
-//     const accessToken = await localStorage.getItem('userAccessKey');
-//     // const token = JSON.parse(accessToken);
-//     console.log('get from localStore', accessToken)
-//     const { data } = await axios.post('/authorisation/checkUserToken', accessToken );
-//     console.log('19 - checkUser', data);
-//     // if(data.status === 'unauthorisate'){
-//     //     registrEl.classList.remove('hidden');  
-//     //     authEl.classList.add('.hidden');
-//     // } else if (data.status === 'ok'){
-//     //     // linkSignIn.classList.add('hidden');
-//     //     window.location.href = '/'
-//     //     // linkSignIn.innerHTML = `<h2>${data.userName}</h2>`;
-//     //     // console.log('after status', data)
-//     // }
-// }
-// checkUserAccess();
-
-// const checkAccessToken = async () => {
-//     const accessToken = await localStorage.getItem('userAccessKey');
-//     const { data } = await axios.post('/authorisation/refreshToken', accessToken );
-//     console.log('20 - checkAcsessToken', data)
-
-// }
-// checkAccessToken();
-
-// const getUserInfo = () => {
-//     const dataJson = localStorage.getItem('user');
-//     const dataUser = JSON.parse(dataJson); 
-//     return dataUser;
-// };
 
 // check auth of user
 const authFormEl = document.forms.auth;
 authFormEl.addEventListener('submit', async (ev) => {
     ev.preventDefault();
+    
     const formData = new FormData(ev.target);
     const { data } = await axios.post('/authorisation/login', formData);
-    console.log('front login:', data);
 
     if(data.status === 'fail authorisation'){
         authEl.innerHTML = `<h3>Uncorrect login or password</h3>`
@@ -60,13 +27,14 @@ registrFormShow.addEventListener('click', (ev) => {
     registrFormShow.classList.add('hidden');
 });
 
+
 // signUp, send user info for create frofile in db
 const signUpEl = document.forms.signup;
 signUpEl.addEventListener('submit', async (ev) => {
     ev.preventDefault();
+    
     const formData = new FormData(ev.target);
     const { data } = await axios.post('/authorisation/signup', formData);
-    console.log('front signup:', data);
 
     if(data.status === 'dublicate_email'){
         registrEl.innerHTML = `<h3>Email is already declarated</h3>`
