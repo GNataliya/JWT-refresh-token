c34c4e1e365afb0684382c4229777f52bc388d7e
GNataliya <80199130+GNataliya@users.noreply.github.com>
2021-10-30 00:39:01 +0300

    Delete test directory

--- test/test.js
+++
@@ -1,7 +0,0 @@
-const authCtrl = require('../controllers/authorisation');
-
-const init = async () => {
-    const result = await authCtrl.login('GNataliya@gggg', 1);
-    console.log(result)
-}
-init();
